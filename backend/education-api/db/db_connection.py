6fa4f68a63fe612eaa76bddbc9c8d881faf26e62
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-12-14 16:51:45 +0530

    🗃️  Added database configuration 🔧

---
+++ backend/education-api/db/db_connection.py
@@ -0,0 +1,8 @@
+from db.session import SessionLocal
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ backend/education-api/db/session.py
@@ -0,0 +1,12 @@
+from core.config import db_url
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = db_url
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
