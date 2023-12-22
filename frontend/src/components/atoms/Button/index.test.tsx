534886653a4758eda94aaafd972db2620d8273c2
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-12-22 11:55:13 +0530

    💚 Fixing CI CD for testing

--- frontend/src/components/atoms/Button/index.test.tsx
+++ frontend/src/components/atoms/Button/index.test.tsx
@@ -8,5 +8,29 @@ describe('MuiButton Tests', () => {
         render(<MuiButton children="Resume" />);
         const buttonElement = screen.getByText("Resume");
         expect(buttonElement).toBeInTheDocument();
-    });  
-})
+    });
+    
+    it("calls onclick function when clicked" , () => {
+        const onClickMock = jest.fn();
+        render(<MuiButton children="Resume" onClick={onClickMock} />);
+        const buttonElement = screen.getByText("Resume");
+        buttonElement.click();
+        expect(onClickMock).toHaveBeenCalledTimes(1);
+    })
+
+    it("disables the button when disabled prop is true", () => {
+        render(<MuiButton disabled={true} children="Disabled" />);
+        const buttonElement = screen.getByText("Disabled");
+        expect(buttonElement).toBeDisabled();
+    });
+    
+    it("applies custom styles to the button", () => {
+        const customStyle = {
+            color: "green",
+            backgroundColor: "black",
+        }
+        render(<MuiButton children="Custom Styled" style={customStyle} />);
+        const buttonElement = screen.getByText("Custom Styled");
+        expect(buttonElement).toHaveStyle(customStyle);
+    });
+})  
