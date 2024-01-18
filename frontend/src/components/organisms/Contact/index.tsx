57eb6894789d3413a8d8055c95f7ca9b7d371b9c
madhuredra <madhuredra.tiwari@zemosolabs.com>
2024-01-18 13:06:38 +0530

    :sparkles: feat : added send message functionality

--- frontend/src/components/organisms/Contact/index.tsx
+++ frontend/src/components/organisms/Contact/index.tsx
@@ -9,23 +9,23 @@ const Contact = () => {
     <StyledContact id={CONTACT}>
       <FormContainer>
         <Form
-          action="https://getform.io/f/57d00a1e-cb8c-494a-9257-d5277f3d6880"
+          action="https://getform.io/f/4a0d40e8-8d29-4e1d-a219-520384176f68"
           method="POST"
           encType="multipart/form-data"
         >
-            <FormGroup>
-                {formFields.map((field) => (
-                    <InputWrapper key={field.field}>
-                        <label htmlFor={field.field}>{field.name}</label>
-                        {field.type === "textarea" ? (
-                            <textarea rows={10} name={field.field} />
-                        ) : (
-                            <input type={field.type} name={field.field} />
-                        )}
-                    </InputWrapper>
-                ))}
-            </FormGroup>
-            <SendButton type="submit">Send Message</SendButton>
+          <FormGroup>
+            {formFields.map(field => (
+              <InputWrapper key={field.field}>
+                <label htmlFor={field.field}>{field.name}</label>
+                {field.type === "textarea" ? (
+                  <textarea rows={10} name={field.field} />
+                ) : (
+                  <input type={field.type} name={field.field} />
+                )}
+              </InputWrapper>
+            ))}
+          </FormGroup>
+          <SendButton type="submit">Send Message</SendButton>
         </Form>
       </FormContainer>
     </StyledContact>
