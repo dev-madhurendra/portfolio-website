8462f1b7ae4f8f328d6fc95d1c66c73dcd2e13e7
dev-madhurendra <madhuredra.tiwari@zemosolabs.com>
2024-02-25 18:04:02 +0530

    :sparkles: feat : added skeleton loader

---
+++ frontend/src/components/molecules/SkeletonLoader/index.tsx
@@ -0,0 +1,28 @@
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+import React from "react";
+import SidebarSkeletonLoader from "../../atoms/SidebarSkeleton";
+import HomeSkeleton from "../../atoms/HomeSkeleton";
+
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+
+
+const PortfolioSkeletonLoader: React.FC = () => {
+  return (
+    <Container>
+      <SidebarSkeletonLoader />
+      <HomeSkeleton />
+    </Container>
+  );
+};
+
+export default PortfolioSkeletonLoader;
