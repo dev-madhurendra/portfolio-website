1eaf3c13fa6cc72c9318fc777a3455b1cefd28cc
dev-madhurendra <madhuredra.tiwari@zemosolabs.com>
2024-02-25 18:01:32 +0530

    :sparkles: feat : added loader skeleton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,56 @@
-import React, { useEffect } from 'react';
-import Navbar from './components/molecules/Navbar';
-import HomeSection from './pages/Home';
-import { DarkModeProvider } from './services/context/DarkMode';
-import { useDarkMode } from './services/customhook/useDarkMode';
-import About from './pages/About';
-import Education from './pages/Education';
-import SkillSection from './pages/Skills';
-import Project from './pages/Project';
-import Experience from './pages/Experience';
-import ContactMeSection from './pages/ContactMe';
-import Footer from './components/organisms/Footer';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/molecules/Navbar";
+import HomeSection from "./pages/Home";
+import { DarkModeProvider } from "./services/context/DarkMode";
+import { useDarkMode } from "./services/customhook/useDarkMode";
+import About from "./pages/About";
+import Education from "./pages/Education";
+import SkillSection from "./pages/Skills";
+import Project from "./pages/Project";
+import Experience from "./pages/Experience";
+import ContactMeSection from "./pages/ContactMe";
+import Footer from "./components/organisms/Footer";
+import "./App.css";
+import PortfolioSkeletonLoader from "./components/molecules/SkeletonLoader";
 
+const AppContent: React.FC = () => {
+  const { isDark } = useDarkMode();
+  const [isLoading, setIsLoading] = useState(true); 
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer); 
+  }, []);
+
+  useEffect(() => {
+    document.body.style.color = isDark ? "#fff" : "#000";
+    document.body.style.fontWeight = isDark ? "500" : "inherit";
+    document.body.style.backgroundColor = isDark ? "#1f1d27" : "#fff";
+  }, [isDark]);
+
+  return (
+    <>
+      {isLoading ? (
+        <PortfolioSkeletonLoader />
+      ) : (
+        <>
+          <Navbar />
+          <HomeSection />
+          <About />
+          <Education />
+          <SkillSection />
+          <Project />
+          <Experience />
+          <ContactMeSection />
+          <Footer />
+        </>
+      )}
+    </>
+  );
+};
 
 function App() {
   return (
@@ -20,29 +59,5 @@ function App() {
     </DarkModeProvider>
   );
 }
-
-const AppContent: React.FC = () => {
-  const { isDark } = useDarkMode();
-
-  useEffect(() => {
-    document.body.style.color = isDark ? '#fff' : '#000',
-    document.body.style.fontWeight = isDark ? '500' : 'inherit',
-    document.body.style.backgroundColor =  isDark ? "#003950" : "#fff"
-  },[isDark])
-
-  return (
-    <>
-      <Navbar />
-      <HomeSection />
-      <About />
-      <Education />
-      <SkillSection />
-      <Project />
-      <Experience />
-      <ContactMeSection />
-      <Footer />
-    </>
-  );
-};
 
 export default App;
